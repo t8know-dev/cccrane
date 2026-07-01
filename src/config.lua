099cb4dab331b9981717b58b601d5c01080bc71b
Rafal Olszewski <rafal.olszewski@hedgehog.com>
2026-07-01 21:54:05 +0200

    Change config

--- src/config.lua
+++ src/config.lua
@@ -38,7 +38,7 @@ return {
 
     -- Modem sides for ECNet2 communication (one per physical computer)
     CLIENT_MODEM_SIDE = "top",    -- crane computer (runs client.lua)
-    SERVER_MODEM_SIDE = "top",    -- panel computer (runs panel.lua, kiosk.lua)
+    SERVER_MODEM_SIDE = "back",    -- panel computer (runs panel.lua, kiosk.lua)
 
     -- Redstone relay output sides (on the relay block)
     AXIS_SIDE = "top",
